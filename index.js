9e3653e116f0b1e81aa8d13f97e48135f716a974
SherAnatoliy <164882963+SherAnatoliy@users.noreply.github.com>
2024-04-22 18:09:06 +0500

    new

---
+++ index.js
@@ -0,0 +1,34 @@
+async function sendRequest(url, method, data) {
+    if(method == "POST") {
+        let response = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+    
+        response = await response.json()
+        return response
+    } else if(method == "GET") {
+        url = url+"?"+ new URLSearchParams(data)
+        let response = await fetch(url, {
+            method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        response = await response.json()
+        return response
+    }
+}
+
+let themeChanger = document.getElementsByClassName("themeChanger")[0]
+themeChanger.addEventListener('click', changeTheme);
+
+function changeTheme() {
+    let body = document.querySelector("body")
+    body.classList.toggle("dark")
+}
